6f94da925258d0e039d6245d3070dccbfa050896
Werner Chacom <wtuj-2021352@kinal.edu.gt>
2023-01-18 14:48:29 -0600

    Primer commit v1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mi primer repositorio
+
+ola gente c:
---
+++ index.js
@@ -0,0 +1,9 @@
+
+function sumar() {
+
+    const valor1 = document.getElementById('valor1');
+    const valor2 = document.getElementById('valor2');
+
+    let suma = parseInt(numero1) + parseInt(numero2);
+    console.log(suma);
+}
